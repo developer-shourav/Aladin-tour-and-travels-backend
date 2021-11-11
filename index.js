c87494b1ed91117e6238584c58004d2c2e0513d0
Mrinmoy Raj <Mrinmoyraj823@gmail.com>
2021-11-11 13:44:17 +0600

    fontand complited

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const { MongoClient } = require('mongodb')
-require('dotenv').config()
-
-const app = express()
-const port = 9000 ;
+const express = require('express');
+const bodyParser = require('body-parser') ;
+const cors = require('cors') ;
+require('dotenv').config();
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p5q9k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -11,19 +11,14 @@ console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-async function run(){
-    try{
-       await client.connect();
-       console.log('database connected');
-    }
+const port = 9000;
+const app = express()
 
-    finally{
-        // await client.close()
-    }
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
-}
 
-run().catch(console.dir);
 
 
 
@@ -31,6 +26,37 @@ app.get('/', (req, res) => {
     res.send('Hello Sir ! Welcome to Aladin Deliver service server...')
 })
 
+
+client.connect(err => {
+    const servicesCollection = client.db("AldinTravels").collection("servicesData");
+   
+    
+    // Add service
+
+    app.post("/addServices", async (req, res) =>{
+        
+        const result =await servicesCollection.insertOne(req.body);
+        
+        res.send(result);
+
+
+    });
+
+
+    //Get All Services 
+    app.get("/tours", async(req, res)=> {
+
+        const result = await servicesCollection.find({}).toArray();
+        res.send(result);
+
+
+    })
+
+
+
+});
+  
+
 app.listen(port, () => {
     console.log(`Running On the port:${port}`)
-})
+});
